05ab4c0f2bfc5ccd1e5f8d71cc15a83102679982
ayushchakraborty111 <ayushchakraborty.cse19@chitkarauniversity.edu.in>
2022-06-01 19:53:00 +0530

    Lives and score of matched cards along with modal alert added

--- Card Picker Game JS/app.js
+++ Card Picker Game JS/app.js
@@ -1,29 +1,101 @@
 let variable = document.querySelectorAll('.img1');
 let count = 0;
+let num1, num2;
+let c1, c2;
 let live = 0;
-let score = document.getElementById('score');
+let attempts = 0, points = 0;
+let score = document.getElementById('span1');
 let life = document.getElementById('lives');
-let arr = ['https://images.unsplash.com/photo-1615162818882-14d8d6330fd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGRpYW9tb25kJTIwY2FyZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60','https://media.istockphoto.com/photos/old-ace-of-spades-isolated-on-white-picture-id184957089?b=1&k=20&m=184957089&s=170667a&w=0&h=5Nm68J_tcXa2pIrL3-FtL7Did22SIgzlgZ0h7eZ3ES8=','https://media.istockphoto.com/photos/king-of-hearts-playing-card-isolated-picture-id1300991991?b=1&k=20&m=1300991991&s=170667a&w=0&h=mc62Qx7r6n7aGib0x10ApM5_JZCt4bGI2Lujk0NpOkQ=','https://media.istockphoto.com/photos/game-cards-picture-id123133624?b=1&k=20&m=123133624&s=170667a&w=0&h=-_yF0YTjxf1H48pdO9aOvFZcdfV2tkm-ljjHhRhG0bc=','https://media.istockphoto.com/photos/playing-cards-picture-id175511561?b=1&k=20&m=175511561&s=170667a&w=0&h=ne__P1kG1y_q3BqVt5JwDzo62zamov4czy78m02FMbk=','https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Playing_card_club_5.svg/614px-Playing_card_club_5.svg.png'];
+let span=document.getElementById("span2");
 
 
-for(let i=0;i<variable.length;i++)
-{
-    let a = Math.floor(Math.random() * arr.length);
-    // console.log(a);
-    variable[i].setAttribute('src', arr[a]);
-    variable[i].setAttribute('style', 'width: 100%; height: 100%;');
+let arr = ['https://media.istockphoto.com/photos/king-of-hearts-playing-card-isolated-picture-id1300991991?b=1&k=20&m=1300991991&s=170667a&w=0&h=mc62Qx7r6n7aGib0x10ApM5_JZCt4bGI2Lujk0NpOkQ=','https://media.istockphoto.com/photos/game-cards-picture-id123133624?b=1&k=20&m=123133624&s=170667a&w=0&h=-_yF0YTjxf1H48pdO9aOvFZcdfV2tkm-ljjHhRhG0bc=','https://media.istockphoto.com/photos/playing-cards-picture-id175511561?b=1&k=20&m=175511561&s=170667a&w=0&h=ne__P1kG1y_q3BqVt5JwDzo62zamov4czy78m02FMbk=','https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Playing_card_club_5.svg/614px-Playing_card_club_5.svg.png','https://media.istockphoto.com/photos/playing-cards-picture-id1335552934?b=1&k=20&m=1335552934&s=170667a&w=0&h=emwaHWisEQKJ5eqcHMIsAEB0tuZn_bd4AvD7yopAlPo=','https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/10_of_diamonds_-_David_Bellot.svg/1200px-10_of_diamonds_-_David_Bellot.svg.png']; 
+
+
+document.getElementById('btn1').onclick = () =>{
+    document.getElementById('winModal').style.display = 'none';
 }
 
-function isMatch()
+
+//Each card access through onclick in html
+function rotate(x, y)
 {
-    for(let i=0;i<variable.length;i++)
+    count++;
+    //console.log(count);
+    shuffle(y);
+    if(count == 1)
     {
-        for(let j=i+1;j<variable.length;j++)
-        {
-            count++;
-        }
+        num1 = x;
+        c1 = y;
+        document.getElementById(num1).style = `transition: 0.7s; transform: rotateY(180deg);height: 120px;width: 120px;`
+    }
+    if(count == 2)
+    {
+        num2 = x;
+        c2 = y;
+        document.getElementById(num2).style = `transition: 0.7s; transform: rotateY(180deg);height: 120px;width: 120px;`
+        setTimeout(flipCard, 800);
+        isMatch(c1, c2);
+        count = 0;
     }
 }
 
 
+//Checking and shuffling random images by id
+function shuffle(y)
+{
+    let imageById = document.getElementById(y);
+    let a = Math.floor(Math.random() * arr.length);
+    imageById.src = arr[a];
+    imageById.style.height = '100%';
+    imageById.style.width = '100%'; 
+}
 
+
+function flipCard()
+{
+    document.getElementById(num1).style = `transition: 2s;transform-style: preserve-3d; transform: rotateY(0deg);height: 120px;width: 120px;`
+    document.getElementById(num2).style = `transition: 2s;transform-style: preserve-3d; transform: rotateY(0deg);height: 120px;width: 120px;`
+}
+
+// console.log(life.textContent);
+// live = life.textContent;
+live = 5;
+
+//click to reload
+document.getElementById('btn2').onclick = ()=>{
+    location.reload();
+}
+
+
+function isMatch(c1, c2)
+{
+   if(document.getElementById(c1).src === document.getElementById(c2).src)
+   {
+       points++;
+       score.innerHTML = "Score " + points;
+       score.style.color = "yellow";
+       setTimeout(()=>{
+            score.style.color = "white";
+       },400)
+       if(points>=1)
+       {
+           document.getElementById('sc1').innerHTML = points;
+           document.getElementById('winModal').style.display = 'block';
+       }   
+       count = 0;
+   }
+   else
+   {
+       live--;
+       if(live < 0)
+       {
+           document.getElementById('gameOverModal').style.display = 'block';
+       }
+        span.innerHTML = "Life " + live; 
+        span.style.color = "red";
+        setTimeout(()=>{
+            span.style.color = "white";
+        },700)             
+   } 
+}
