271e481536dfcc819f827fb44b4e964505000e66
ayushchakraborty111 <ayushchakraborty.cse19@chitkarauniversity.edu.in>
2022-06-02 11:27:06 +0530

    Styling and modal alert added

--- Card Picker Game JS/app.js
+++ Card Picker Game JS/app.js
@@ -1,7 +1,7 @@
 let variable = document.querySelectorAll('.img1');
 let count = 0;
 let num1, num2;
-let c1, c2;
+let image1, image2;
 let live = 0;
 let attempts = 0, points = 0;
 let score = document.getElementById('span1');
@@ -9,7 +9,7 @@ let life = document.getElementById('lives');
 let span=document.getElementById("span2");
 
 
-let arr = ['https://media.istockphoto.com/photos/king-of-hearts-playing-card-isolated-picture-id1300991991?b=1&k=20&m=1300991991&s=170667a&w=0&h=mc62Qx7r6n7aGib0x10ApM5_JZCt4bGI2Lujk0NpOkQ=','https://media.istockphoto.com/photos/game-cards-picture-id123133624?b=1&k=20&m=123133624&s=170667a&w=0&h=-_yF0YTjxf1H48pdO9aOvFZcdfV2tkm-ljjHhRhG0bc=','https://media.istockphoto.com/photos/playing-cards-picture-id175511561?b=1&k=20&m=175511561&s=170667a&w=0&h=ne__P1kG1y_q3BqVt5JwDzo62zamov4czy78m02FMbk=','https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Playing_card_club_5.svg/614px-Playing_card_club_5.svg.png','https://media.istockphoto.com/photos/playing-cards-picture-id1335552934?b=1&k=20&m=1335552934&s=170667a&w=0&h=emwaHWisEQKJ5eqcHMIsAEB0tuZn_bd4AvD7yopAlPo=','https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/10_of_diamonds_-_David_Bellot.svg/1200px-10_of_diamonds_-_David_Bellot.svg.png']; 
+let arr = ['https://media.istockphoto.com/photos/king-of-hearts-playing-card-isolated-picture-id1300991991?b=1&k=20&m=1300991991&s=170667a&w=0&h=mc62Qx7r6n7aGib0x10ApM5_JZCt4bGI2Lujk0NpOkQ=','https://media.istockphoto.com/photos/playing-cards-picture-id175511561?b=1&k=20&m=175511561&s=170667a&w=0&h=ne__P1kG1y_q3BqVt5JwDzo62zamov4czy78m02FMbk=','https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Playing_card_club_5.svg/614px-Playing_card_club_5.svg.png','https://media.istockphoto.com/photos/playing-cards-picture-id1335552934?b=1&k=20&m=1335552934&s=170667a&w=0&h=emwaHWisEQKJ5eqcHMIsAEB0tuZn_bd4AvD7yopAlPo=','https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/10_of_diamonds_-_David_Bellot.svg/1200px-10_of_diamonds_-_David_Bellot.svg.png']; 
 
 
 document.getElementById('btn1').onclick = () =>{
@@ -18,33 +18,33 @@ document.getElementById('btn1').onclick = () =>{
 
 
 //Each card access through onclick in html
-function rotate(x, y)
+function rotate(cardId, imageId)
 {
     count++;
     //console.log(count);
-    shuffle(y);
+    shuffle(imageId);
     if(count == 1)
     {
-        num1 = x;
-        c1 = y;
+        num1 = cardId;
+        image1 = imageId;
         document.getElementById(num1).style = `transition: 0.7s; transform: rotateY(180deg);height: 120px;width: 120px;`
     }
     if(count == 2)
     {
-        num2 = x;
-        c2 = y;
+        num2 = cardId;
+        image2 = imageId;
         document.getElementById(num2).style = `transition: 0.7s; transform: rotateY(180deg);height: 120px;width: 120px;`
         setTimeout(flipCard, 800);
-        isMatch(c1, c2);
+        isMatch(image1, image2);
         count = 0;
     }
 }
 
 
 //Checking and shuffling random images by id
-function shuffle(y)
+function shuffle(imageId)
 {
-    let imageById = document.getElementById(y);
+    let imageById = document.getElementById(imageId);
     let a = Math.floor(Math.random() * arr.length);
     imageById.src = arr[a];
     imageById.style.height = '100%';
@@ -68,9 +68,9 @@ document.getElementById('btn2').onclick = ()=>{
 }
 
 
-function isMatch(c1, c2)
+function isMatch(image1, image2)
 {
-   if(document.getElementById(c1).src === document.getElementById(c2).src)
+   if(document.getElementById(image1).src === document.getElementById(image2).src)
    {
        points++;
        score.innerHTML = "Score " + points;
@@ -82,6 +82,7 @@ function isMatch(c1, c2)
        {
            document.getElementById('sc1').innerHTML = points;
            document.getElementById('winModal').style.display = 'block';
+           document.getElementById('main-div').style.pointerEvents = 'none';
        }   
        count = 0;
    }
@@ -91,6 +92,8 @@ function isMatch(c1, c2)
        if(live < 0)
        {
            document.getElementById('gameOverModal').style.display = 'block';
+           document.getElementById('main-div').style.pointerEvents = 'none';
+           live = 0;
        }
         span.innerHTML = "Life " + live; 
         span.style.color = "red";
